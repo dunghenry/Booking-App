9f9005bbcd0f490512ba0daf17da871bb73b6e3b
dunghenry <trandungksnb6930082001@gmail.com>
2022-06-22 03:24:25 +0700

    add use realtime db, firestore

--- auth-crud/src/firebase/index.js
+++ auth-crud/src/firebase/index.js
@@ -4,19 +4,23 @@ import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider } from "firebase/auth";
 import { FacebookAuthProvider } from "firebase/auth";
 import { GithubAuthProvider } from "firebase/auth";
+import { getDatabase } from 'firebase/database'
+import {getStorage } from 'firebase/storage'
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_SORAGE_BUCKET,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 const facebookProvider = new FacebookAuthProvider()
 const googleProvider = new GoogleAuthProvider();
 const githubProvider = new GithubAuthProvider();
-export { auth, db, facebookProvider, googleProvider, githubProvider};
+export { auth, database, db, storage, facebookProvider, googleProvider, githubProvider};
--- auth-crud/src/pages/home/index.jsx
+++ auth-crud/src/pages/home/index.jsx
@@ -1,6 +1,57 @@
 import React from 'react'
+import { database, db, storage } from '../../firebase';
+// import { ref, onValue, set } from 'firebase/database';
+import {collection, addDoc } from 'firebase/firestore'
 
+import { ref, getMetadata  } from "firebase/storage";
+import avatar from '../../images/avatar.jpg'
 const Home = () => {
+
+  // React.useEffect(() => {
+  //   const posts = ref(database, 'posts')
+  //   onValue(posts, (snapshot) => {
+  //     const data = snapshot.val()
+  //     console.log(data)
+  //   })
+  // }, [])
+
+  // React.useEffect(() => {
+  //   set(ref(database, 'posts'), {
+  //     username: "TranDung",
+  //     email: "trandungksnb00@gmail.com",
+  //     description: "Xin chao"
+  //   });
+  // }, [])
+
+
+  // React.useEffect(() => {
+  //   async function demo() {
+  //     try {
+  //       const docRef = await addDoc(collection(db, "users"), {
+  //         first: "Ada",
+  //         last: "Lovelace",
+  //         born: 1815
+  //       });
+  //       console.log("Document written with ID: ", docRef.id);
+  //     } catch (e) {
+  //       console.error("Error adding document: ", e);
+  //     }
+  //   }
+  //   demo()
+  // }, [])
+
+  React.useEffect(() => {
+    // console.log(storage)
+    const imagesRef = ref(storage, 'images');
+  //   const avatarRef = ref(storage, 'avatar.jpg');
+  //   getMetadata(avatarRef)
+  // .then((metadata) => {
+  //   console.log(metadata)
+  // })
+  // .catch((error) => {
+  //   // Uh-oh, an error occurred!
+  // });
+  }, [])
   return (
     <div className="container-fluid">Home</div>
   )
